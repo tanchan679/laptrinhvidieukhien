b830ae0adb1498f145ed845121d60efb134ae76c
tanchan679 <tanchan679@gmail.com>
2022-05-11 09:49:14 +0700

    them ham delay timer counter

---
+++ Tao ham delay banwg timer couter.c
@@ -0,0 +1,26 @@
+#include <REGX52.H>
+
+void delay(unsigned int t)
+{
+	do{
+		//KHOI tao gia tri ban dau cho thanh ghi Timer0
+		TL0 = 0X18;	 
+		TH0 = 0xfc;		  //=.64536 => Thanh ghi 16 bit co gia tri toi da 65536, 100 xung nhip thuc chip thuc hien trong 1ms
+
+		TR0=1;//Khoi dong timer0
+		while(TF0!=0);// co chan bo nho bang 1
+		TR0=1;//dong lai timer0
+		t--;
+	}  while(t>0);
+}
+
+void main(){//ctr chinh
+     TMOD &=0XF0;//xOA CAC BIT DIEU KHIEN T0 trong thanh ghi TMOD
+	 TMOD |= 0x01//Chon mode cho timer0
+
+	 while(1)
+	 {
+	 	//thuc hien cai gi do
+		 delay(100);
+	 }
+}
